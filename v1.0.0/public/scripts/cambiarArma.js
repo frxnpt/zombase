91c6e3b1134d688682d6ec0538c872fb707f5fac
AntonioRuiz96 <78626950+AntonioRuiz96@users.noreply.github.com>
2021-05-17 16:24:50 +0200

    Limpiado codigo de funciones de cambiar arma / imagen

--- v1.0.0/public/scripts/cambiarArma.js
+++ v1.0.0/public/scripts/cambiarArma.js
@@ -3,20 +3,12 @@ function cambiarArma() {// cambio de imagen (y modo disparo!) Para el cambio de 
     switch (Jugador.armaActual) {
         case 1:
             Jugador.armaActual = 2;
-            armaAutomatica = false;//default
+            armaAutomatica = false;//default. Sera puesta true en cambiarimagenpersonaje
             document.getElementById("municion").innerHTML = "Cargador: " + Jugador.arma2.cargadorArma2 + " / " + Jugador.arma2.municionArma2;
             if (Jugador.arma2.nombreArma2 == "none") {
                 document.getElementById("personaje").setAttribute("src", "../public/resources/personajes/RAMBO.png");
-            } else if (Jugador.arma2.nombreArma2 == "PISTOLA1") {
-                cambiarImagenPersonaje("PISTOLA1");
-            } else if (Jugador.arma2.nombreArma2 == "ESCOPETA1") {
-                cambiarImagenPersonaje("ESCOPETA1");
-            } else if (Jugador.arma2.nombreArma2 == "RIFLEASALTO1") {
-                cambiarImagenPersonaje("RIFLEASALTO1");
-            } else if (Jugador.arma2.nombreArma2 == "PISTOLA2") {
-                cambiarImagenPersonaje("PISTOLA2");
-            } else if (Jugador.arma2.nombreArma2 == "ARMARAYOS") {
-                cambiarImagenPersonaje("ARMARAYOS");
+            } else {
+                cambiarImagenPersonaje(Jugador.arma2.nombreArma2);
             }
 
             document.getElementById("armamano2").style.opacity = 1;
@@ -24,20 +16,12 @@ function cambiarArma() {// cambio de imagen (y modo disparo!) Para el cambio de 
             break;
         case 2:
             Jugador.armaActual = 1;
-            armaAutomatica = false;//default
+            armaAutomatica = false;//default. Sera puesta true en cambiarimagenpersonaje
             document.getElementById("municion").innerHTML = "Cargador: " + Jugador.arma1.cargadorArma1 + " / " + Jugador.arma1.municionArma1;
-            if (Jugador.arma1.nombreArma1 == "PISTOLA1") {
-                cambiarImagenPersonaje("PISTOLA1");
-            } else if (Jugador.arma1.nombreArma1 == "none") {
+            if (Jugador.arma1.nombreArma1 == "none") {
                 document.getElementById("personaje").setAttribute("src", "../public/resources/personajes/RAMBO.png");
-            } else if (Jugador.arma1.nombreArma1 == "ESCOPETA1") {
-                cambiarImagenPersonaje("ESCOPETA1");
-            } else if (Jugador.arma1.nombreArma1 == "RIFLEASALTO1") {
-                cambiarImagenPersonaje("RIFLEASALTO1");
-            } else if (Jugador.arma1.nombreArma1 == "PISTOLA2") {
-                cambiarImagenPersonaje("PISTOLA2");
-            } else if (Jugador.arma1.nombreArma1 == "ARMARAYOS") {
-                cambiarImagenPersonaje("ARMARAYOS");
+            } else {
+                cambiarImagenPersonaje(Jugador.arma1.nombreArma1);
             }
 
             document.getElementById("armamano1").style.opacity = 1;
--- v1.0.0/public/scripts/cambiarImagenPersonaje.js
+++ v1.0.0/public/scripts/cambiarImagenPersonaje.js
@@ -2,7 +2,5 @@ function cambiarImagenPersonaje(nombre) {//Cambio de imagen cuando el jugador co
     if (nombre == "RIFLEASALTO1") {
         armaAutomatica = true;//modo de disparo automatico permitido
     }
-    if (Jugador.skinActual == "RAMBO") {//separar if, asignar skin a variable y meterla a los string, ahorrando codigo
-        document.getElementById("personaje").setAttribute("src", "../public/resources/personajes/RAMBO_" + nombre + ".png");
-    }//Añadir mas armas en el futuro y skins del personaje si hay
+    document.getElementById("personaje").setAttribute("src", "../public/resources/personajes/" + Jugador.skinActual + "_" + nombre + ".png");
 }
