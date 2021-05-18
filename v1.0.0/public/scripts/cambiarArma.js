81990ee0c33cbda94094913b705c3d47ea809be0
frxnpt <62038055+frxnpt@users.noreply.github.com>
2021-05-18 11:40:11 +0200

    change img path on gun change

--- v1.0.0/public/scripts/cambiarArma.js
+++ v1.0.0/public/scripts/cambiarArma.js
@@ -1,14 +1,14 @@
-function cambiarArma() {// cambio de imagen (y modo disparo!) Para el cambio de arma con la ruleta del raton
+function cambiarArma() { // cambio de imagen (y modo disparo!) Para el cambio de arma con la ruleta del raton
     if (pausa == false) {
 
-        event.preventDefault();//TODO: Actualizar a cambio con variable para evitar repetición de código (igual que cambio imagen)
+        event.preventDefault(); //TODO: Actualizar a cambio con variable para evitar repetición de código (igual que cambio imagen)
         switch (Jugador.armaActual) {
             case 1:
                 Jugador.armaActual = 2;
-                armaAutomatica = false;//default. Sera puesta true en cambiarimagenpersonaje
+                armaAutomatica = false; //default. Sera puesta true en cambiarimagenpersonaje
                 document.getElementById("municion").innerHTML = "Cargador: " + Jugador.arma2.cargadorArma2 + " / " + Jugador.arma2.municionArma2;
                 if (Jugador.arma2.nombreArma2 == "none") {
-                    document.getElementById("personaje").setAttribute("src", "../public/resources/personajes/RAMBO.png");
+                    document.getElementById("personaje").setAttribute("src", "../resources/personajes/RAMBO.png");
                 } else {
                     cambiarImagenPersonaje(Jugador.arma2.nombreArma2);
                 }
@@ -18,10 +18,10 @@ function cambiarArma() {// cambio de imagen (y modo disparo!) Para el cambio de 
                 break;
             case 2:
                 Jugador.armaActual = 1;
-                armaAutomatica = false;//default. Sera puesta true en cambiarimagenpersonaje
+                armaAutomatica = false; //default. Sera puesta true en cambiarimagenpersonaje
                 document.getElementById("municion").innerHTML = "Cargador: " + Jugador.arma1.cargadorArma1 + " / " + Jugador.arma1.municionArma1;
                 if (Jugador.arma1.nombreArma1 == "none") {
-                    document.getElementById("personaje").setAttribute("src", "../public/resources/personajes/RAMBO.png");
+                    document.getElementById("personaje").setAttribute("src", "../resources/personajes/RAMBO.png");
                 } else {
                     cambiarImagenPersonaje(Jugador.arma1.nombreArma1);
                 }
