1ddb55c0d088627009d1a535ccfbfc16c69e66f4
frxnpt <62038055+frxnpt@users.noreply.github.com>
2021-05-18 11:25:03 +0200

    create app.js

---
+++ v1.0.0/app.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const mysql = require("mysql");
+const path = require("path");
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './config.env' })
+
+const app = express();
+
+const db = mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE
+});
+
+const publicDirectory = path.join(__dirname, './public');
+console.log(__dirname);
+app.use(express.static(publicDirectory));
+
+app.set('view engine', 'hbs');
+
+db.connect((error) => {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log("MYSQL Connected...")
+    }
+})
+
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+app.get("/about-us", (req, res) => {
+    res.render("about-us");
+});
+app.listen(5069, () => {
+    console.log("Server started on Port 5069")
+})
