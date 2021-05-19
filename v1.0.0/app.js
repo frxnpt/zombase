e614c8784ce618e5398dc1e07aa8bf3750ea8263
frxnpt <62038055+frxnpt@users.noreply.github.com>
2021-05-19 12:53:24 +0200

    create session on app.js to store data on it

--- v1.0.0/app.js
+++ v1.0.0/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mysql = require("mysql");
 const path = require("path");
 const dotenv = require('dotenv');
+const session = require('express-session')
 const cookieParser = require('cookie-parser');
 
 dotenv.config({ path: './config.env' })
@@ -36,6 +37,21 @@ db.connect((error) => {
     }
 })
 
+
+app.use(session({
+    secret: 'aosdnkasdnk1.ksadAMsamdlkasmdaks',
+    resave: false,
+    saveUninitialized: false,
+}));
+
+// Run a "middleware" function on each request
+app.use((req, res, next) => {
+    // Check if we've already initialised a session
+    console.log(req.session.hola);
+    req.session.hola = "hola";
+    session.Store.hola;
+    next();
+});
 
 app.use('/', require('./routes/pages'));
 app.use('/auth/', require('./routes/auth'));
