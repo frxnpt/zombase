6b9aa16c5d0e2a4bc1f6a79cf8c1332d3ebfcb68
frxnpt <62038055+frxnpt@users.noreply.github.com>
2021-05-19 22:55:22 +0200

    disable caching on whole application

--- v1.0.0/app.js
+++ v1.0.0/app.js
@@ -28,7 +28,13 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.set('view engine', 'hbs');
+// caching disabled for every route
+app.use(function(req, res, next) {
+    res.set('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0');
+    next();
+});
 
+// otherwise put the res.set() call into the route-handler you want
 db.connect((error) => {
     if (error) {
         console.log(error);
