82fbba4d6c94f02ac799317a2d28c77d44b9a9bc
frxnpt <62038055+frxnpt@users.noreply.github.com>
2021-05-20 15:34:51 +0200

    change the way the pause menu appears

--- v1.0.0/public/scripts/pausa.js
+++ v1.0.0/public/scripts/pausa.js
@@ -1,27 +1,11 @@
 function pausar() {
     pausa = true;
     clearInterval(disparoAutomatico);
-    var menuPausa = document.createElement("div");
-    menuPausa.setAttribute("id", "menu");
-    menuPausa.setAttribute("class", "menu");
-    var texto = document.createTextNode("PAUSA");
-    menuPausa.appendChild(texto);
-    var botonContinuar = document.createElement("p");
-    var textoBoton = document.createTextNode("¿Continuar?");
-    botonContinuar.appendChild(textoBoton);
-    botonContinuar.setAttribute("onclick", "reanudar()");
-    botonContinuar.setAttribute("class", "botonContinuar");
-    menuPausa.appendChild(botonContinuar);
-    var botonReiniciar = document.createElement("p");
-    var textoBoton2 = document.createTextNode("Reiniciar");
-    botonReiniciar.appendChild(textoBoton2);
-    botonReiniciar.setAttribute("onclick", "resetear()");
-    botonReiniciar.setAttribute("class", "botonReiniciar");
-    menuPausa.appendChild(botonReiniciar);
-    container.appendChild(menuPausa);
-    var menu = document.getElementById("menu");
-    menu.style.left = container.getBoundingClientRect().width / 2 -
-        menu.getBoundingClientRect().width / 2 + "px";
-    menu.style.top = container.getBoundingClientRect().height / 2 -
-        menu.getBoundingClientRect().height / 2 + "px";
+    var menuPausa = document.getElementById("menuPausa");
+    menuPausa.style.display = "inherit"
+
+    menuPausa.style.left = container.getBoundingClientRect().width / 2 -
+        menuPausa.getBoundingClientRect().width / 2 + "px";
+    menuPausa.style.top = container.getBoundingClientRect().height / 2 -
+        menuPausa.getBoundingClientRect().height / 2 + "px";
 }
