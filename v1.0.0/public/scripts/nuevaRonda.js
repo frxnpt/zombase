c227f0846c1973035f9c588fb3b65038310ff71d
AntonioRuiz96 <78626950+AntonioRuiz96@users.noreply.github.com>
2021-05-24 17:56:15 +0200

    Terrenos cambian cada 5 rondas

--- v1.0.0/public/scripts/nuevaRonda.js
+++ v1.0.0/public/scripts/nuevaRonda.js
@@ -1,6 +1,16 @@
 function nuevaRonda(intervalo) {
     document.getElementById("ronda").innerHTML = "Ronda: " + ronda;
     clearTimeout(intervalo);
+    switch (ronda / 5) {//Cambia terreno cada 5 rondas
+        case 1:
+            document.getElementById("terreno").setAttribute("src", "./resources/terrenos/TERRENO2.png")
+            break;
+        case 2:
+            document.getElementById("terreno").setAttribute("src", "./resources/terrenos/TERRENO3.png")
+            break;
+        default:
+            break;
+    }
     contadorIntervalos++;
     generarZombie = setInterval(crearZombie, 2500 - (20 * ronda));
 }
