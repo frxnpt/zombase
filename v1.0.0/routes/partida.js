652bd6da41c2c1fcb94e149cf0482c07e5b7ce4f
frxnpt <62038055+frxnpt@users.noreply.github.com>
2021-05-28 10:44:23 +0200

    Change the route to show top players

--- v1.0.0/routes/partida.js
+++ v1.0.0/routes/partida.js
@@ -4,4 +4,8 @@ const partidaController = require('../controllers/partida');
 const router = express.Router();
 
 router.post("/guardar", partidaController.guardar);
+
+router.get("/top", partidaController.verTop);
+
+
 module.exports = router;
