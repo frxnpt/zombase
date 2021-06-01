ae599df0f4d8ce187239a2620afd4f6167417c4f
AntonioRuiz96 <78626950+AntonioRuiz96@users.noreply.github.com>
2021-06-01 11:28:54 +0200

    Actualizada comparación para el disparo automático

--- v1.0.0/public/scripts/cambiarImagenPersonaje.js
+++ v1.0.0/public/scripts/cambiarImagenPersonaje.js
@@ -1,5 +1,5 @@
-function cambiarImagenPersonaje(nombre) { //Cambio de imagen cuando el jugador coge arma
-    if (nombre == "M16" || nombre == "FAMAS" || nombre == "MP5" || nombre == "MAC-9") {
+function cambiarImagenPersonaje(nombre, modoDisparo) { //Cambio de imagen cuando el jugador coge arma
+    if (modoDisparo == 2) {
         armaAutomatica = true; //modo de disparo automatico permitido
     } else {
         armaAutomatica = false;
