cc6e5ed24dbf75a046256fa1537d74a88933cf31
frxnpt <62038055+frxnpt@users.noreply.github.com>
2021-06-07 22:32:24 +0200

    Comment the code and format a little bit the js functions

--- v1.0.0/app.js
+++ v1.0.0/app.js
@@ -1,3 +1,4 @@
+//Importar todas las liberías necesarias
 const express = require("express");
 const mysql = require("mysql");
 const path = require("path");
@@ -5,11 +6,12 @@ const dotenv = require('dotenv');
 const session = require('express-session')
 const cookieParser = require('cookie-parser');
 
-
+//Localizamos el archivo con las contraseñas e información sensible
 dotenv.config({ path: './config.env' })
-
+    //Guardamos la funcion que crea la applicación express en una variable
 const app = express();
 
+//creo una conexión con la base de datos y la guardo en una variable
 const db = mysql.createConnection({
     host: process.env.DATABASE_HOST,
     user: process.env.DATABASE_USER,
@@ -17,6 +19,7 @@ const db = mysql.createConnection({
     database: process.env.DATABASE
 });
 
+//establecemos el directorio public como directorio principal
 const publicDirectory = path.join(__dirname, 'public');
 console.log(__dirname);
 app.use(express.static(publicDirectory));
@@ -28,14 +31,11 @@ app.use(express.urlencoded({ extended: false }))
 app.use(express.json());
 app.use(cookieParser());
 
+//Establecemos el sistema de templates (Handlebars)
 app.set('view engine', 'hbs');
-// caching disabled for every route
-app.use(function(req, res, next) {
-    res.set('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0');
-    next();
-});
 
-// otherwise put the res.set() call into the route-handler you want
+/*en caso de que haya un error con la conexión mostramos el error, y si no hay error, mostramos un mensaje por consola 
+para verificar que todo está bien*/
 db.connect((error) => {
     if (error) {
         console.log(error);
@@ -56,14 +56,26 @@ app.use((req, res, next) => {
     // Check if we've already initialised a session
     req.session.hola = "hola";
     session.Store.hola;
+        if (error) {
+            console.log(error);
+        } else {
+            console.log("MYSQL Connected...")
+        }
+    })
+    // caching disabled for every route
+app.use(function(req, res, next) {
+    res.set('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0');
     next();
 });
-
+//añadimos todas las rutas desde otra carpeta
 app.use('/', require('./routes/pages'));
 app.use('/auth/', require('./routes/auth'));
 app.use('/partida/', require('./routes/partida'));
 app.use('/perfil/', require('./routes/perfil'));
 
+
+
+//seleccionamos el puerto en el que va a correr nuestra applicación
 app.listen(5069, () => {
     console.log("Server started on Port 5069")
 })
