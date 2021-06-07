06a45e8dd79cbb278ad807103a4ed68fe79c0108
frxnpt <62038055+frxnpt@users.noreply.github.com>
2021-06-07 23:17:05 +0200

    Remove useless code

--- v1.0.0/public/scripts/guardarPartida.js
+++ v1.0.0/public/scripts/guardarPartida.js
@@ -5,17 +5,8 @@ function guardarPartida() {
         return;
     }
     var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState == 4) {
-            if (xhr.status == 200) {
-                document.getElementById("testrequest").textContent = xhr.responseText;
-            }
-            if (xhr.status == 404) {
-                console.log("file or resource not found");
-            }
-        }
-    }
     var today = new Date();
+
     var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
     var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
     var dateTime = date + ' ' + time;
